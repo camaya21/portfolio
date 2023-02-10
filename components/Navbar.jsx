489bde57f9fb8916235d3ea9cf01bc9ac3093061
camaya21 <cindymamaya21@gmail.com>
2023-02-10 00:28:58 -0500

    Added Navbar logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
     <div className='fixed w-full h-20 shadow-xl z-[100]'>
         <div className='felx justify-between items-center w-full h-full px-2 2xl:px-16'>
-            <Image src="" alt="/" />
+            <Image 
+            src="/../public/assets/CAS.png" 
+            alt="/" 
+            width='170' 
+            height='50'
+            />
         </div>
     </div>
   )
