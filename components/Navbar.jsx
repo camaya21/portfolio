3af9f54634bce8512220108760d861a5e00fedfa
camaya21 <cindymamaya21@gmail.com>
2023-02-09 23:27:16 -0500

    cleaned up code and added basic tailwind css

---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='fixed w-full h-20 shadow-xl z-[100]'>
+        <div className='felx justify-between items-center w-full h-full px-2 2xl:px-16'>
+            <Image src="" alt="/" />
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
