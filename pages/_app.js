6f5914390c675ceaddfdff46a4c37311e88db099
camaya21 <cindymamaya21@gmail.com>
2023-02-17 00:03:31 -0500

    Adding GA and fixing issues

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,44 @@
 import '@/styles/globals.css'
+import Script from 'next/script'
+import { useRouter } from 'next/router';
+import { useEffect } from "react";
+import * as gtag from "../lib/gtag"
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+        router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"/>
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html:`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-KRG25R39D2', {
+              page_path: window.location.pathname,
+          });
+        `,
+      }}
+    />
+    <Component {...pageProps} />
+    </>
+  )
 }
+
+export default MyApp
