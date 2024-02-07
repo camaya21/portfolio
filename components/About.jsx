d93b44054d1ba2a6efc7c8173cbb8b516b80a815
camaya21 <cindymamaya21@gmail.com>
2024-02-06 19:51:32 -0500

    Added to About section

--- components/About.jsx
+++ components/About.jsx
@@ -9,9 +9,18 @@ const About = () => {
             <div className='col-span-2'>
                 <p className='uppercase text-xl tracking-widest text-[#5651e5]'>About</p>
                 <h2 className='py-4'>Who I Am</h2>
-                <p className='text-lg py-2 text-gray-600'>I have always looked up to women in STEM and thought they were so powerful, so I gave myself the opportunity to become the type of person I've looked up to. My tech interest started in highschool when I enjoyed making youtube videos and mini movies on my iphone. Since then I have always been driven to create memorable things that speak to people in different ways.</p>
-                <p className='text-lg py-2 text-gray-600'>As a graduate from GA's Software Engineering BootCamp, I've cultivated my technical abilities to be skilled at swiftly and efficiently acquiring new knowledge and concepts. I'm able to work well under pressure and time constraints, along with being able to clearly and effectively communicate. I believe my background as a retail associate has made me a strong worker and implemented life long competencies.</p>
-                <p className='text-lg py-2 text-gray-600'>I enjoy learning and growing with each season that passes. More cool things about me are that I'm a dog mom, a baker in my free time, and wannabe singer in my solitude. I like reading books and exploring the outdoors. Lastly I love movies with great cinematography and acting.</p>
+                <p className='text-lg py-2 text-gray-600'>I have always looked up to women in STEM and thought they were so powerful, so I gave myself the opportunity to 
+                  become the type of person I've looked up to. My tech interest started in highschool when I enjoyed making youtube videos and mini movies on my iphone. 
+                  Since then I have always been driven to create memorable things that speak to people in different ways.</p>
+                <p className='text-lg py-2 text-gray-600'> I am a full-time student juggling academics with a temporary teaching role in my county. 
+                  My experience is a dynamic blend of learning, leadership, and community engagement. Balancing the demands of education while stepping into 
+                  the role of a teacher has provided me with a unique perspective on the intersection of learning and real-world application. As a graduate from 
+                  GA's Software Engineering BootCamp, I've cultivated my technical abilities to be skilled 
+                  at swiftly and efficiently acquiring new knowledge and concepts. I'm able to work well under pressure and time constraints, along with being able to 
+                  clearly and effectively communicate. My experiences have shaped me into a resilient, 
+                  adaptable, and compassionate individual. It's taught me the value of perseverance, the importance of empathy, and the power of education to transform lives.</p>
+                <p className='text-lg py-2 text-gray-600'>I enjoy learning and growing with each season that passes. More cool things about me are that I'm a dog mom, 
+                  a baker in my free time, and wannabe singer in my solitude. I like reading books and exploring the outdoors. Lastly I love movies with great cinematography and acting.</p>
                 <p className='text-lg py-2 text-gray-600'>"In case I don't see ya, good afternoon, good evening, and good night" - The Truman Show</p>
             </div>
             <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
